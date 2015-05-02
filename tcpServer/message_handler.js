797156eeda4e3dded3c0ad786bd3b4a6c920db82
blakemcconnell <blakemcconnell@gmail.com>
2015-05-02 16:02:50 -0700

    handle messages from cinder to max

--- tcpServer/message_handler.js
+++ tcpServer/message_handler.js
@@ -11,17 +11,27 @@ util.inherits( MessageHandler, EventEmitter );
 
 //handle incoming messages from server
 MessageHandler.prototype.handle_incoming = function( obj ) {
+	console.log( "received msg: ", obj, "from sender: ", obj.sender );
 	var recipients = [];
 	var outgoing = {};
 	outgoing.type = null;
+	//handle messages with destination cinder
 	if ( obj.type == "to_cinder" ) {
 		recipients.push( "cinder" );
-		outgoing.recipients = recipients;
 		outgoing.type = "from_max";
 		outgoing.data = obj.data;
 		outgoing.data_type = "float";
 	}
+	//handle messages with destination max
+	else if ( obj.type == "to_max" ) {
+		recipients.push( "max" );
+		outgoing = obj;
+		outgoing.type = "from_cinder";
+		console.log( "sending message: ", obj );
+	}
+	//filter malformed messages
 	if ( outgoing.type != null ) {
+		outgoing.recipients = recipients;
 		this.write( outgoing );
 	}
 }
