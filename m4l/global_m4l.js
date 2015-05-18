618c9418b39949a43d0f5ff023a52ef38f56f8c6
blakemcconnell <blakemcconnell@gmail.com>
2015-05-17 20:19:54 -0700

    rename global max for live object

--- m4l/global_m4l.js
+++ m4l/global_m4l.js
@@ -7,16 +7,16 @@
    Simplex noise: https://gist.github.com/banksean/304522.
 */
 
-g = new Global("m4l");
+GM4L = new Global("m4l");
 
 //performs a drunk walk, arguments are min and max of output
-g.Drunk = function(min_out, max_out) {
+GM4L.Drunk = function(min_out, max_out) {
 	this.prev_val = (min_out + max_out)/2;
 	this.min_out = min_out;
 	this.max_out = max_out;
 }
 	//arguments set max and min step size
-	g.Drunk.prototype.step = function(min_step, max_step) {
+	GM4L.Drunk.prototype.step = function(min_step, max_step) {
 		sim_noise = new gp.SimplexNoise();
 		var new_val;
 		var rand_val = sim_noise.noise(1., 1.) * (max_step - min_step + 1) + max_step;
@@ -38,7 +38,7 @@ g.Drunk = function(min_out, max_out) {
 	}
 
 //implementation of common easing functions
-g.Easing = function(total_time, rate) {
+GM4L.Easing = function(total_time, rate) {
 	this.total_time = total_time;
 	this.rate = rate; //rate of bangs sent to js object
 	this.duration = Math.floor(total_time/rate);
@@ -46,23 +46,23 @@ g.Easing = function(total_time, rate) {
 	this.last_map_val = 0;
 }
 	
-	g.Easing.prototype.start_motion = function() {
+	GM4L.Easing.prototype.start_motion = function() {
 		this.counter = 0;
 	}
 	
 	//set start val and end val
-	g.Easing.prototype.set_start_val = function(start_val) {
+	GM4L.Easing.prototype.set_start_val = function(start_val) {
 		var sv = start_val;
 		return sv;
 	}
 	
-	g.Easing.prototype.set_end_val = function(end_val) {
+	GM4L.Easing.prototype.set_end_val = function(end_val) {
 		var ev = end_val;
 		return ev;
 	}
 	
 	//arguments: easing_function(counter/duration, start_val, end_val), end val
-	g.Easing.prototype.move = function(easing_function, end_val) {
+	GM4L.Easing.prototype.move = function(easing_function, end_val) {
 		var mapped_value;
 		if (this.counter < this.duration) {
 			var mapped_value = easing_function;
@@ -78,25 +78,25 @@ g.Easing = function(total_time, rate) {
 		return mapped_value;
 	}
 	
-	g.Easing.prototype.set_rate = function(new_rate) {
+	GM4L.Easing.prototype.set_rate = function(new_rate) {
 		this.rate = new_rate;
 		this.duration = this.total_time/this.rate;
 		this.counter = (this.total_time/this.rate) + 1;
 	}
 	
-	g.Easing.prototype.linear = function( t, v1, v2 ) {
+	GM4L.Easing.prototype.linear = function( t, v1, v2 ) {
   		return v1 + t * (v2 - v1);
 	}
 	
-	g.Easing.prototype.easeIn = function( t, v1, v2 ) {
+	GM4L.Easing.prototype.easeIn = function( t, v1, v2 ) {
   		return v1 + (t * t * t) * (v2 - v1);
 	}
 	
-	g.Easing.prototype.easeOut = function( t, v1, v2 ) {
+	GM4L.Easing.prototype.easeOut = function( t, v1, v2 ) {
   		return v1 + (((t-1) * (t-1) * (t-1)) + 1) * (v2 - v1);
 	}
 	
-	g.Easing.prototype.easeInOut = function( t, v1, v2 ) {
+	GM4L.Easing.prototype.easeInOut = function( t, v1, v2 ) {
   		if ((t * 2) < 1) {
     		return v1 + ((t * 2) * (t * 2) * (t * 2)) * (v2 - v1) / 2.0;
 		}
@@ -105,14 +105,14 @@ g.Easing = function(total_time, rate) {
 		}
 	}
 
-g.ParamChange = function(devices, parameters) {
+GM4L.ParamChange = function(devices, parameters) {
 	this.devices = devices;
 	this.parameters = parameters;
 	this.min = 0;
 	this.max = 0;
 }
 	//use parameter list to populate umenu, or print names to console
-	g.ParamChange.prototype.list_params = function(api_object) { //pass name of api object
+	GM4L.ParamChange.prototype.list_params = function(api_object) { //pass name of api object
 		api_object.goto("this_device canonical_parent devices " + this.devices);
 		var args = arrayfromargs( api_object.get ( "parameters" ) );
 		var output = {};
@@ -142,36 +142,36 @@ g.ParamChange = function(devices, parameters) {
 		return output;
 	}
 	
-	g.ParamChange.prototype.set_devices = function(device_number) {
+	GM4L.ParamChange.prototype.set_devices = function(device_number) {
 		this.devices = device_number;
 	}
 	
-	g.ParamChange.prototype.set_params = function(param_number) {
+	GM4L.ParamChange.prototype.set_params = function(param_number) {
 		this.parameters = param_number;
 	}
 	
 	//set minimum and maximum value, use for scaling
-	g.ParamChange.prototype.set_bounds = function(api_object) {
+	GM4L.ParamChange.prototype.set_bounds = function(api_object) {
 		api_object.goto("this_device canonical_parent devices " + this.devices + " parameters " + this.parameters);
 		this.min = api_object.get("min");
 		this.max = api_object.get("max");
 	}
 	
-	g.ParamChange.prototype.set_value = function(api_object, value) {
+	GM4L.ParamChange.prototype.set_value = function(api_object, value) {
 		api_object.goto("this_device canonical_parent devices " + this.devices + " parameters " + this.parameters);
 		api_object.set("value", value);
 	}
 
 /*js version of transport, still a little wonky
 initialize api object: var tempo = api.get("tempo");*/
-g.Transport = function(tempo) {
+GM4L.Transport = function(tempo) {
 	this.last_bar = 0;
 	this.last_beat = 0;
 	this.bpm_ms = 60000./tempo;
 }
 	
 	//get current time: api.call("get_current_beats_song_time");
-	g.Transport.prototype.beat_bang = function(cur_bt_st) {
+	GM4L.Transport.prototype.beat_bang = function(cur_bt_st) {
 		var song_time = cur_bt_st.split(".");
 		var beat_change;
 			if (song_time[1] !== this.last_beat){ 
@@ -184,7 +184,7 @@ g.Transport = function(tempo) {
 		return beat_change;
 	}
 	
-	g.Transport.prototype.bar_bang = function(cur_bt_st) {
+	GM4L.Transport.prototype.bar_bang = function(cur_bt_st) {
 		var song_time = cur_bt_st.split(".");
 		var bar_change;
 			if (song_time[0] !== this.last_bar){ 
