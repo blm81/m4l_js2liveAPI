96c5d4ec942f38c5e986ac85441cdb1573aad847
blakemcconnell <blakemcconnell@gmail.com>
2015-06-14 16:24:26 -0700

    load JSON from .txt config file into Max

---
+++ m4l/config_loader.js
@@ -0,0 +1,40 @@
+/*
+	load JSON configuration text (.txt) file
+*/
+
+inlets = 1;
+outlets = 1;
+
+//load external text file (with .txt extension)
+function load_file( file_path ) 
+{
+	var ret_val = "";
+
+	//max-specific variables
+	filename = file_path;
+	access = "readwrite";
+	typelist = new Array("iLaF" , "maxb" , "TEXT" );
+
+	//loop through file and add contents to return string
+	file = new File( filename, access, typelist );
+
+	while( file.isopen && file.position < file.eof ){
+		ret_val += file.readline().trim(); //trim whitespace and special chars
+	}
+
+	file.close();
+
+	return ret_val;
+}
+
+function anything()
+{
+	var args = arrayfromargs( messagename, arguments );
+
+	switch( args[0] ) {
+		case 'read':
+			//output json from config file
+			outlet( 0, "json " + load_file( args[1] ) );
+			break;
+	}
+}
