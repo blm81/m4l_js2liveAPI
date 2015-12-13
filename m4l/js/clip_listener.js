12ccfdcd42e29ce63bcc94d75a232e55858a07d9
Blake McConnell <blakemcconnell@gmail.com>
2015-12-13 11:51:34 -0800

    emit json message on clip stop and start events

--- m4l/js/clip_listener.js
+++ m4l/js/clip_listener.js
@@ -10,7 +10,16 @@ trackObserver.property = "playing_slot_index";
 
 function clipTriggerListener( args )
 {
-	var argArr = arrayfromargs( args );
+	/** output object : 
+		type = String
+		data object : event = String
+			slotNum : num, number of playing slot
+	*/				 
+	var objOut = {
+			type : "clip_event",
+			data : {}
+		},
+		argArr = arrayfromargs( args );
 
 	//index 1 is the track number
 	switch( argArr[1] ) {
@@ -18,11 +27,16 @@ function clipTriggerListener( args )
 		//-2 for clip off
 		case -2: 
 			post( "clip off" + '\n' );
+			objOut.data.event = "clip_off";
 			outlet( 1, "bang" );
 			break;
 		default:
 			post( "clip on: " + argArr[1] + '\n' );
+			objOut.data.event = "clip_on";
+			objOut.data.slotNum = argArr[1];
 			outlet( 0, argArr[1] );
 			break;
 	}
+	//receiver name : clip event
+	messnamed( 'clip_event', JSON.stringify( objOut ));
 }
