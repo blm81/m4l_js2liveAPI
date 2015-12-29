489e891f6ab6f44560e96d8a0628b9d8e93bab56
Blake McConnell <blakemcconnell@gmail.com>
2015-12-28 17:57:58 -0800

    set center value and range dynamically

--- m4l/js/ramp_in_range.js
+++ m4l/js/ramp_in_range.js
@@ -10,7 +10,7 @@ var centerVal = 0.9,		//center value of total output range
 	lastVal = centerVal,
 	range = 0.2,			//total output range
 	rate = 30,				//rate / duration are in milliseconds
-	duration = 3000,
+	duration = 1000,
 	drunk = new GM4L.Drunk( centerVal - ( range / 2 ), centerVal + ( range / 2 )),
 	easing = new GM4L.Easing( duration, rate );
 
@@ -21,11 +21,31 @@ function anything()
 	switch( a[0] ) {
 
 		//start ramp
-		case 'start':
+		case "start":
 			easing.start_val = lastVal;
 			easing.end_val = drunk.step( 0.01, 0.05 ); //TODO set this dynamically
 			lastVal = easing.end_val;
 			easing.start();
+			break;
+
+		case "set_center_val":
+			if ( a[1] ) {
+				centerVal = a[1];
+				resetDrunk();
+			}
+			else {
+				post( "you must supply an argument to set center value" );
+			}
+			break;
+
+		case "set_range":
+			if ( a[1] ) {
+				range = a[1];
+				resetDrunk();
+			}
+			else {
+				post( "you must supply an argument to set the range" );
+			}
 			break;
 	}
 }
@@ -36,4 +56,10 @@ function bang()
 	if ( easing.end_val && easedVal !== easing.end_val ) {
 		outlet( 0, easedVal );
 	}
+}
+
+function resetDrunk()
+{
+	drunk.setMinOut( centerVal - ( range / 2 ));
+	drunk.setMaxOut( centerVal + ( range / 2 ));
 }
